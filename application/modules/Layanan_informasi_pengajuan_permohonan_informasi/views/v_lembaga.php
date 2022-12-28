e8fe4e310b91368488981f02ff064d6004a98965
Widya Yuristika Oktavia <yuristikawidya@gmail.com>
2022-12-28 10:54:42 +0700

    Update v_lembaga.php

--- application/modules/Layanan_informasi_pengajuan_permohonan_informasi/views/v_lembaga.php
+++ application/modules/Layanan_informasi_pengajuan_permohonan_informasi/views/v_lembaga.php
@@ -110,53 +110,52 @@
                                         </div>
                                         <div class="col-lg-12 col-sm-12">
                                             <div class="form-group">
-                                                <form action="" method="POST" enctype="multipart/form-data">
-                                                    <label for="identitas_pemohon">Identitas Pemohon</label>
-                                                    <div class="form-control">
-                                                        <input type="file" name="identitas_pemohon" id="identitas_pemohon" accept="image/png, image/jpeg, image/jpg, image/gif">
-                                                    </div>
-                                            </div>
-                                            <div class="col-lg-12 col-sm-12">
-                                                <div class="form-group">
-                                                    <form action="" method="POST" enctype="multipart/form-data">
-                                                        <label for="akta_pendirian">Akta Pendirian dari Kemenkumham</label>
-                                                        <div class="form-control">
-                                                            <input type="file" name="akta_pendirian" id="akta_pendirian" accept="image/png, image/jpeg, image/jpg, image/gif">
-                                                        </div>
+                                                <label for="identitas_pemohon">Identitas Pemohon</label>
+                                                <div class="form-control">
+                                                    <input type="file" name="identitas_pemohon" id="identitas_pemohon" accept="image/png, image/jpeg, image/jpg, image/gif">
                                                 </div>
-                                                <div class="col-lg-12 col-sm-12">
-                                                    <div class="form-group">
-                                                        <form action="" method="POST" enctype="multipart/form-data">
-                                                            <label for="sk_bakesbangpol">Surat Keterangan terdaftar Bakesbangpol Jatim</label>
-                                                            <div class="form-control">
-                                                                <input type="file" name="sk_bakesbangpol" id="sk_bakesbangpol" accept="image/png, image/jpeg, image/jpg, image/gif">
-                                                            </div>
-                                                    </div>
-                                                    <div class="col-lg-12 col-sm-12">
-                                                        <div class="form-group">
-                                                            <form action="" method="POST" enctype="multipart/form-data">
-                                                                <label for="sk_domisili">Surat Keterangan Domisili</label>
-                                                                <div class="form-control">
-                                                                    <input type="file" name="sk_domisili" id="sk_domisili" accept="image/png, image/jpeg, image/jpg, image/gif">
-                                                                </div>
-                                                        </div>
-                                                        <div class="col-lg-12 col-sm-12">
-                                                            <div class="form-group">
-                                                                <form action="" method="POST" enctype="multipart/form-data">
-                                                                    <label for="ormas">AD/ ART Lembaga Publik/ Ormas </label>
-                                                                    <div class="form-control">
-                                                                        <input type="file" name="ormas" id="ormas" accept="image/png, image/jpeg, image/jpg, image/gif">
-                                                                    </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-lg-12 col-md-12">
-                                                            <button type="submit" class="default-btn">
-                                                                Send Message
-                                                            </button>
-                                                            <div id="msgSubmit" class="h3 text-center hidden"></div>
-                                                            <div class="clearfix"></div>
-                                                        </div>
-                                                    </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 col-sm-12">
+                                            <div class="form-group">
+                                                <label for="akta_pendirian">Akta Pendirian dari Kemenkumham</label>
+                                                <div class="form-control">
+                                                    <input type="file" name="akta_pendirian" id="akta_pendirian" accept="image/png, image/jpeg, image/jpg, image/gif">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 col-sm-12">
+                                            <div class="form-group">
+                                                <label for="sk_bakesbangpol">Surat Keterangan terdaftar Bakesbangpol Jatim</label>
+                                                <div class="form-control">
+                                                    <input type="file" name="sk_bakesbangpol" id="sk_bakesbangpol" accept="image/png, image/jpeg, image/jpg, image/gif">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 col-sm-12">
+                                            <div class="form-group">
+                                                <label for="sk_domisili">Surat Keterangan Domisili</label>
+                                                <div class="form-control">
+                                                    <input type="file" name="sk_domisili" id="sk_domisili" accept="image/png, image/jpeg, image/jpg, image/gif">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 col-sm-12">
+                                            <div class="form-group">
+                                                <label for="ormas">AD/ ART Lembaga Publik/ Ormas </label>
+                                                <div class="form-control">
+                                                    <input type="file" name="ormas" id="ormas" accept="image/png, image/jpeg, image/jpg, image/gif">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 col-md-12">
+                                            <button type="submit" class="default-btn">
+                                                Send Message
+                                            </button>
+                                            <div id="msgSubmit" class="h3 text-center hidden"></div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </div>
                                 </form>
                             </div>
                         </div>
